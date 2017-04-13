9352356fa027c1bd885880adab30c9d4ff67f357
Kaluchii <mr.kaluchii@mail.ru>
2017-04-13 15:03:22 +0600

    Merge branches

--- resources/views/front/news/news.blade.php
+++ resources/views/front/news/news.blade.php
@@ -5,18 +5,15 @@
     <?php $title = $static->page_title_field ?>
     <article class="content akcii" data-page="stock">
         {{--<div class="content-head">--}}
-            {{--<h1 class="content-head-title spec">{{$static->title_field}}</h1>--}}
-            {{--<div class="grid grid-pad head-grid">--}}
-                {{--<div class="col-1-2 text-block">--}}
-                    {{--{!! $static->descriptions_field !!}--}}
-                {{--</div>--}}
-                {{--<div class="col-1-2">--}}
-                {{--</div>--}}
-            {{--</div>--}}
+        {{--<h1 class="content-head-title spec">{{$static->title_field}}</h1>--}}
+        {{--<div class="grid grid-pad head-grid">--}}
+        {{--<div class="col-1-2 text-block">--}}
+        {{--{!! $static->descriptions_field !!}--}}
         {{--</div>--}}
-
-        <h1 class="content-head-title stock-title">Акции и спецпредложения</h1>
-
+        {{--<div class="col-1-2">--}}
+        {{--</div>--}}
+        {{--</div>--}}
+        {{--</div>--}}
         <div class="grid grid-pad akcii flex-grid">
 
             <?php $i= 0;
@@ -34,63 +31,35 @@
                 return $str;
             }
             ?>
-            @foreach($dom->special_offers_group as $item)
-                <?php $i++; ?>
-                @if($i <= 1)
-
-                    <div class="col_1-3">
-                        <div class="stock-item">
-                            <a href="/service/ul-trazvukovoj-smas-lifting" class="shares-link">
-                                <img src="/images/special_offers_prev_664_primary.jpg" alt="">
-                            </a>
-                            <h2 class="shares-name">Скидка 20% на <a href="/service/ul-trazvukovoj-smas-lifting" class="link">процедуры SMAS-лифтинга</a>
-                            </h2>
-                            <p class="shares-description">Новогодние скидки 15% на коррекцию зон лица, декольте и шеи с применением препарата TEOSYAL от женевской лаборатории TEOXANE.</p>
-                            <p class="shares-date">Каждую субботу и воскресенье</p>
+            <div class="rows">
+                @foreach($dom->special_offers_group as $item)
+                    <?php $i++; ?>
+                    @if($i <= 3)
+                        <div class="row">
+                            <div class="col-9-12">
+                                <h2 class="shares-name">
+                                    <?php $link = wrap_in_link($item->prev_name_field, $item->slug_field); ?>
+                                    {!! $link !!}
+                                </h2>
+                                <p class="shares-description">{{$item->prev_text_field}}</p>
+                            </div>
+                            <div class="col-3-12">
+                                <a href="{{$item->slug_field}}" class="shares-link">
+                                    <img src="/images/{{$item->prev_image->primary_link}}" alt="">
+                                    <p class="shares-date">{{$item->all_text_field}}</p>
+                                </a>
+                            </div>
                         </div>
-                        <div class="stock-item stock-social">
-                            <img src="/img/stock-social.png" alt="">
-                            <p class="stock-social-descr">Получайте акции через WhatApp по номеру <span class="no-br">+7 777 178-29-26</span>
-                                и в&nbsp;нашем <a href="//facebook.com" class="stock-social-link">Facebook</a></p>
-                        </div>
-                    </div>
-                    <div class="col_1-3">
-                        <div class="stock-item">
-                            <a href="/news/skidka-8-procentov" class="shares-link">
-                                <img src="/images/special_offers_prev_663_primary.jpg" alt="">
-                            </a>
-                            <h2 class="shares-name">8% скидка <a href="/news/skidka-8-procentov" class="link">в первый визит</a>
-                            </h2>
-                            <p class="shares-description">Вступите в нашу группу в Facebook, поставьте лайк под любой публикацией и получите скидку в 8% при вашем первом посещении клиники.</p>
-                            <p class="shares-date">При первом посещении</p>
-                        </div>
-                        <div class="stock-item">
-                            <a href="/service/massazh" class="shares-link">
-                                <img src="/images/special_offers_prev_165_primary.jpg" alt="">
-                            </a>
-                            <h2 class="shares-name"><a href="/service/massazh" class="link">Антицеллюлитный массаж</a> со скидкой 20%
-                            </h2>
-                            <p class="shares-description">Антицеллюлитный массаж помогает справиться с нарушением структуры подкожно-жировой клетчатки, вследствие которого ухудшается эластичность тканей, меняется цвет и рельеф кожи.</p>
-                            <p class="shares-date">До 30 мая</p>
-                        </div>
-                    </div>
-                    <div class="col_1-3 distributed">
-                        <div class="stock-item">
-                            <a href="/service/massazh" class="shares-link">
-                                <img src="/images/special_offers_prev_165_primary.jpg" alt="">
-                            </a>
-                            <h2 class="shares-name"><a href="/service/massazh" class="link">Антицеллюлитный массаж</a> со скидкой 20%
-                            </h2>
-                            <p class="shares-description">Антицеллюлитный массаж помогает справиться с нарушением структуры подкожно-жировой клетчатки, вследствие которого ухудшается эластичность тканей, меняется цвет и рельеф кожи.</p>
-                            <p class="shares-date">До 30 мая</p>
-                        </div>
-                    </div>
-
-                @endif
-            @endforeach
-
-
+                    @endif
+                @endforeach
+            </div>
+            <div class="stock-info">
+                <div class="stock-social">
+                    <img src="/img/stock-social.png" alt="" class="stock-social-img">
+                    <p class="stock-social-descr">Получайте акции через WhatApp по номеру <span class="no-br">+7 777 178-29-26</span>
+                        и в&nbsp;нашем <a href="//facebook.com" class="stock-social-link">Facebook</a></p>
+                </div>
+            </div>
         </div>
-
     </article>
 @endsection
